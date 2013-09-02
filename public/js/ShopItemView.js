b9fefdb1f6583e086c3fcdae935c78ce12c43456
Darren Cato <darren.cato@oracle.com>
2013-09-02 11:25:24 -0400

    Lecture 12 understanding and controlling events

--- public/js/ShopItemView.js
+++ public/js/ShopItemView.js
@@ -2,14 +2,33 @@ window.ShopItemView = Backbone.View.extend({
 	tagName: "tr",
 	template: "#shopitemView",
 	events: {
-		'click .dropdown-menu a': 'setCategory'
+		'click .dropdown-menu a': 'setCategory',
+		'click .purchase': 'purchase',
+		'click .delete': 'destroy'
 	},
 
 	initialize: function(){
-		_.bindAll(this, 'render');
+		_.bindAll(this, 'render','destroy','purchase');
 		this.model.bind('change', this.render);
 		this.options.categories.bind('all', this.render);
 	},
+
+	destroy: function(e){
+		e.preventDefault();
+		this.model.destroy();
+	},
+
+	purchase: function(e){
+		e.preventDefault();
+		this.model.off();
+		this.model.set('purchased', true);
+		this.model.save();
+		var el = this.el;
+		$(this.el).fadeOut('slow', function(e){
+			$(el).remove();
+		});
+	},
+
 
 	setCategory: function(e){
 		e.preventDefault();
