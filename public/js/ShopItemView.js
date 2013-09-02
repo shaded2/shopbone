ba8942a8cda684b6099a6bb195ebff5dc1583c54
Darren Cato <darren.cato@oracle.com>
2013-09-02 10:29:48 -0400

    lecture 10 - binding models to views

--- public/js/ShopItemView.js
+++ public/js/ShopItemView.js
@@ -1,18 +1,29 @@
 window.ShopItemView = Backbone.View.extend({
 	tagName: "tr",
 	template: "#shopitemView",
-	events: {},
+	events: {
+		'click .dropdown-menu a': 'setCategory'
+	},
 
 	initialize: function(){
 		_.bindAll(this, 'render');
 		this.model.bind('change', this.render);
+		this.options.categories.bind('all', this.render);
+	},
+
+	setCategory: function(e){
+		e.preventDefault();
+		var id = $(e.target).data('id');
+		var c = this.options.categories.get(id);
+		this.model.set('category', c);
+		this.model.save();
 	},
 
 	render: function(){
 		$(this.el).html(
 			_.template(
 				$(this.template).html(),
-				{ model: this.model}
+				{ model: this.model, categories:this.options.categories }
 			)
 		);
 		return this;
